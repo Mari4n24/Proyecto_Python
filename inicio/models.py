8bd72c55c605e9ef33ea941af10b42b6bbf44755
Mari <marur00911@gmail.com>
2025-11-06 16:21:17 -0300

    Agregar modelo 'Colectividad' con atributos 'pais', 'comida_tipica' y 'baile_tipico'

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Colectividad(models.Model):
+    def __init__(self, pais, comida_tipica, baile_tipico):
+        self.pais = pais
+        self.comida_tipica = comida_tipica
+        self.baile_tipico = baile_tipico
