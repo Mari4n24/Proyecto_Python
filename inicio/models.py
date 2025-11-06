33e64815a50c42c0fcb05235d4b39f2f3107ab77
Mari <marur00911@gmail.com>
2025-11-06 16:32:08 -0300

    Agregar configuración de URL para incluir las vistas 'inicio' y 'about'

--- inicio/models.py
+++ inicio/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Colectividad(models.Model):
-    def __init__(self, pais, comida_tipica, baile_tipico):
+    def __init__(self, nombre_titular, pais, capital, comida_tipica, baile_tipico, cantidad_de_participantes, informacion):
+        self.nombre_titular = nombre_titular
         self.pais = pais
+        self.capital = capital
         self.comida_tipica = comida_tipica
         self.baile_tipico = baile_tipico
+        self.cantidad_de_participantes = cantidad_de_participantes
+        self.informacion = informacion
