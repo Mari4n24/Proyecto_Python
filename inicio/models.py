52bcc9c6643eeb264470d24f4137b24e17be08f8
Mari <marur00911@gmail.com>
2025-11-07 15:58:09 -0300

    Eliminar métodos innecesarios y limpiar el modelo 'Colectividad' en 'models.py'; agregar migración para los campos 'nombre' y 'pais'.

--- inicio/models.py
+++ inicio/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
 class Colectividad(models.Model):
-    nombre = models.CharField(max_length=100)  # Ej: "Pedro"
-    pais = models.CharField(max_length=100)    # Ej: "Venezuela"
-
-    def __str__(self):
-        return self.nombre
+    nombre = models.CharField(max_length=100) 
+    pais = models.CharField(max_length=100)    
