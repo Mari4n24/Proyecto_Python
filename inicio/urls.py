c1d4394492f09c7291cb547b63ebaa3f29f65a59
Mari <marur00911@gmail.com>
2025-11-18 15:41:59 -0300

    Refactor plantillas HTML para incluir estructura básica y navegación coherente

--- inicio/urls.py
+++ inicio/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from inicio.views import inicio, about, crear_colectividad, listar_colectividades
 
 urlpatterns = [
-    path('', inicio),
-    path('about/', about),
-    path('crear-colectividad/<str:nombre>/<str:pais>/', crear_colectividad),
-    path('listar-colectividades/', listar_colectividades),
+    path('', inicio, name='inicio'),
+    path('about/', about, name='about'),
+    path('crear-colectividad/<str:nombre>/<str:pais>/', crear_colectividad, name='crear_colectividad'),
+    path('listar-colectividades/', listar_colectividades, name='listar_colectividades'),
 ]
